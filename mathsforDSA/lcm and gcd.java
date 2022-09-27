b445cf74277257c529103c3c8fbb58befc2c1414
Revan Patil <108737061+revanpatil@users.noreply.github.com>
2022-09-27 22:57:34 +0530

    Create lcm and gcd.java

---
+++ mathsforDSA/lcm and gcd.java
@@ -0,0 +1,23 @@
+package io.github.jiangdequan;
+
+public class GCDandLcm {
+public static void main(String[] args) {
+    System.out.println(gcd(4,9));
+    System.out.println(lcm(45,230)); 
+
+}   
+
+static int gcd(int a,int b){   
+    if(a == 0){
+        return b;
+    }
+    return gcd(b%a, a);       
+}
+
+static int lcm(int a, int b){
+    return a*b/gcd(a,b);
+}
+
+}
+
+ 
