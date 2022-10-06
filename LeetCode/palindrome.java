e52efd029825557fa1979077c63cedb94f16f5d3
Revan Patil <108737061+revanpatil@users.noreply.github.com>
2022-10-06 22:56:20 +0530

    Create palindrome.java

---
+++ LeetCode/palindrome.java
@@ -0,0 +1,24 @@
+//https://leetcode.com/problems/valid-palindrome/submissions/
+
+
+class Solution {
+     public boolean isPalindrome(String s) {
+        StringBuilder sb = new StringBuilder(s.toLowerCase());
+        for(int i = 0, j = sb.length() - 1; i < sb.length() - 1;){
+            if((sb.charAt(i) >= 97 && sb.charAt(i) <= 122) || (sb.charAt(i) >= 48 && sb.charAt(i) <=57)){
+                if( (sb.charAt(j) >= 97 &&  sb.charAt(j) <= 122) || (sb.charAt(j) >= 48 && sb.charAt(j) <=57)){
+                    if(sb.charAt(i) != sb.charAt(j)){
+                        return false;
+                    }
+                    i++;
+                    j--;
+                }else{
+                    j--;
+                }
+            }else{
+                i++;
+            }
+        }
+        return true;
+    }
+}
