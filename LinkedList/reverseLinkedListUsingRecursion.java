19497c8ce524410166816f46bfb07b9c20aae2ca
Revan Patil <108737061+revanpatil@users.noreply.github.com>
2022-11-12 13:44:46 +0530

    Create reverseLinkedListUsingRecursion.java

---
+++ LinkedList/reverseLinkedListUsingRecursion.java
@@ -0,0 +1,43 @@
+Intuition
+Approach
+CC : Kunal Kushwaha
+image.png
+
+Complexity
+Time complexity:
+O(N)
+Space complexity:
+O(1)
+Code
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        if(head == null){
+            return head;
+        }
+        ListNode prev = null;
+        ListNode present = head;
+        ListNode next = present.next;
+
+        while(present!=null){
+
+            present.next = prev;
+            prev = present.next;
+            prev = present;
+            present = next;
+            if(next != null){
+                next = next.next;
+            }
+        }
+        return prev;
+    }
+}
